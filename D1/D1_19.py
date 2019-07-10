04a93f184c0eb7ddc3f19eab45db0f5dd63b1823
young31 <migael38317@gmail.com>
2019-07-11 00:06:32 +0900

    revise previous upload

--- D1/D1_19.py
+++ D1/D1_19.py
@@ -11,4 +11,12 @@ for i in range(n):
     print('#{0} {1}'.format(i+1, sum))
 
 
-
+# young 127ms, 176
+T = input()
+for i in range(1,4) :
+    b = list(map(int, input().split( )))
+    t=0
+    for j in b :
+        if j % 2 ==1 :
+            t += j
+    print("#{0} {1}".format(i, t))
--- make_file.py
+++ make_file.py
@@ -1,5 +1,5 @@
-for i in range(1, 4): # 범위주의!!!!!!!!!!!!!
-    f = open(f'D1_0{i}.py', 'w')
+for i in range(1, 3): # 범위주의!!!!!!!!!!!!!
+    f = open('D1_%02d.py'%i, 'w')
     f.write('# young\n')
 # 파일명 치고 파일 다수 만들기
 # 사용시 있는 파일 안 없어지게 조심할 것
@@ -7,4 +7,4 @@ for i in range(1, 4): # 범위주의!!!!!!!!!!!!!
 
 # 경로 지정예시
 # for i in range(1, 4):
-#     f = open(f'C:/Users/young/Desktop/git/Algorithm/INT/INT03_0{i}.py', 'w')
+#     f = open('C:/Users/young/Desktop/git/Algorithm/INT/INT03_%02d.py'%i, 'w')
