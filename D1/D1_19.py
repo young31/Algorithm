080187860a7c5436976a33e09ebbec10b4cd7738
young31 <migael38317@gmail.com>
2019-07-11 09:17:26 +0900

    young edit

--- D1/D1_19.py
+++ D1/D1_19.py
@@ -20,3 +20,9 @@ for i in range(1,4) :
         if j % 2 ==1 :
             t += j
     print("#{0} {1}".format(i, t))
+
+# young 125ms, 140
+for i in range(1, int(input()) + 1):
+    n = list(filter(lambda x : x % 2 ==1, list(map(int, input().split( )))))
+    print('#%d'%i, sum(n))
+## 코드는 짧아지는데 실행속도는 못따라가네요ㅠ
