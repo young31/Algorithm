08ee27fa3a2b692a01183f9244af9c3f47f70903
young31 <migael38317@gmail.com>
2019-07-18 17:56:45 +0900

    young edit

--- D2/D2_08.py
+++ D2/D2_08.py
@@ -1,1 +1,13 @@
-# young
+# young 131ms, 267
+T = int(input())
+ 
+for i in range(1,T+1) :
+    t = int(input())
+    n =[]
+    for j in range(0,t) :
+        a = input().split( )
+        n.append(a[0]*int(a[1]))
+    n = ''.join(n)
+    print('#%d'%i)
+    for k in range(0,len(n)//10 +1) :
+        print(n[10*k:10*k+10])
--- D2/D2_24.py
+++ D2/D2_24.py
@@ -1,1 +1,7 @@
-# young
+# young 115ms, 127
+for i in range(1, int(input())+1):
+    t = input()
+    k = 1
+    while t[0:k] != t[k:2*k]:
+        k += 1
+    print('#%d'%i, k)
