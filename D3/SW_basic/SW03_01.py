b5187059ccc767575ae0a9bd4ab983b3064c7662
young31 <migael38317@gmail.com>
2019-07-21 20:16:08 +0900

    누락문제추가

--- D3/SW_basic/SW03_01.py
+++ D3/SW_basic/SW03_01.py
@@ -1,15 +1,6 @@
-# young 151ms, 411
+# young 125ms, 113
 for i in range(1, 11):
-    t = int(input())
-    mat = []
-    n = 0
-    for j in range(8):
-        mat.append(list(input()))
-    mat2 = list(zip(*mat))
-    for j in range(8):
-        for k in range(9-t):
-            if list(mat[j][k:k+t]) == list(reversed(mat[j][k:k+t])):
-                n += 1
-            if list(mat2[j][k:k+t]) == list(reversed(mat2[j][k:k+t])):
-                n += 1
-    print('#%d'%i, n)
+    input()
+    test = (input())
+    long = (input())
+    print('#%d'%i, long.count(test))
--- D3/SW_basic/SW03_02.py
+++ D3/SW_basic/SW03_02.py
@@ -1,18 +1,15 @@
-# young 16122ms, 498
-def find(x, y):
-    for t in range(100, 0, -1):
-        for j in range(100):
-            for k in range(101 - t):
-                if list(x[j][k:k + t]) == list(reversed(x[j][k:k + t])):
-                    return t
-                if list(y[j][k:k + t]) == list(reversed(y[j][k:k + t])):
-                    return t
- 
- 
+# young 151ms, 411
 for i in range(1, 11):
     t = int(input())
     mat = []
-    for j in range(100):
+    n = 0
+    for j in range(8):
         mat.append(list(input()))
     mat2 = list(zip(*mat))
-    print('#%d'%i, find(mat, mat2))
+    for j in range(8):
+        for k in range(9-t):
+            if list(mat[j][k:k+t]) == list(reversed(mat[j][k:k+t])):
+                n += 1
+            if list(mat2[j][k:k+t]) == list(reversed(mat2[j][k:k+t])):
+                n += 1
+    print('#%d'%i, n)
